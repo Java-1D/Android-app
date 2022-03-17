4c25eefc903d42836a910c40f0861e7d9542b342
Yongkang <chiayongkang@hotmail.com>
2022-03-17 14:54:38 +0800

    ViewEventActivity

--- app/src/main/java/com/example/myapplication2/ViewEventsActivity.java
+++ app/src/main/java/com/example/myapplication2/ViewEventsActivity.java
@@ -1,14 +1,106 @@
 package com.example.myapplication2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.myapplication2.objectmodel.EventModel;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class ViewEventsActivity extends AppCompatActivity {
+
+    private static final String TAG = "Test";
+    private FirebaseFirestore firebaseFirestore;
+    private RecyclerView eventsList; // providing views that represent items in a data set.
+    private FirestoreRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_events);
+
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        eventsList = findViewById(R.id.events_list);
+
+        // Query
+        Query query = firebaseFirestore.collection("Event");
+        Log.d(TAG, query.toString());
+
+        // RecyclerOptions
+        FirestoreRecyclerOptions<EventModel> options = new FirestoreRecyclerOptions.Builder<EventModel>()
+                .setQuery(query, EventModel.class)
+                .build();
+
+        // Recyler Adapter
+        adapter = new FirestoreRecyclerAdapter<EventModel, EventViewHolder>(options) {
+            @NonNull
+            @Override
+            public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                // Creates a new instance of View Holder
+                // Uses layout called R.layout.event_item
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_item, parent, false);
+                return new EventViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull EventViewHolder holder, int position, @NonNull EventModel model) {
+                // binds query object  (TestModel) to recycler view TestViewHolder
+                Log.d(TAG, model.toString());
+                Log.d(TAG, String.valueOf(holder));
+                holder.event_title.setText(model.getTitle());
+                holder.user_created.setText(model.getUserCreated());
+
+            }
+
+
+        };
+
+        eventsList.setHasFixedSize(true);
+        eventsList.setLayoutManager(new LinearLayoutManager(this));
+        eventsList.setAdapter(adapter);
+
+
+    }
+    // View Holder Class
+
+    private class EventViewHolder extends RecyclerView.ViewHolder {
+
+        // Declare Text view and set data
+
+        private TextView event_title;
+        private TextView user_created;
+
+        // passed in the item from oncreate
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            event_title = (TextView) itemView.findViewById(R.id.event_title);
+            user_created = (TextView) itemView.findViewById(R.id.user_created);
+        }
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
---
+++ app/src/main/java/com/example/myapplication2/objectmodel/EventModel.java
@@ -0,0 +1,148 @@
+package com.example.myapplication2.objectmodel;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class EventModel {
+    private int capacity;
+    private String description;
+    Date eventCreated;
+    Date eventEnd;
+    Date eventStart;
+    String imagePath;
+    String module;
+    String status;
+    String title;
+    String userCreated;
+    ArrayList<String> userJoined;
+    String venue;
+
+    public EventModel(int capacity, String description, Date eventCreated, Date eventEnd, Date eventStart, String imagePath, String module, String status, String title, String userCreated, ArrayList<String> userJoined, String venue) {
+        this.capacity = capacity;
+        this.description = description;
+        this.eventCreated = eventCreated;
+        this.eventEnd = eventEnd;
+        this.eventStart = eventStart;
+        this.imagePath = imagePath;
+        this.module = module;
+        this.status = status;
+        this.title = title;
+        this.userCreated = userCreated;
+        this.userJoined = userJoined;
+        this.venue = venue;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getEventCreated() {
+        return eventCreated;
+    }
+
+    public void setEventCreated(Date eventCreated) {
+        this.eventCreated = eventCreated;
+    }
+
+    public Date getEventEnd() {
+        return eventEnd;
+    }
+
+    public void setEventEnd(Date eventEnd) {
+        this.eventEnd = eventEnd;
+    }
+
+    public Date getEventStart() {
+        return eventStart;
+    }
+
+    public void setEventStart(Date eventStart) {
+        this.eventStart = eventStart;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUserCreated() {
+        return userCreated;
+    }
+
+    public void setUserCreated(String userCreated) {
+        this.userCreated = userCreated;
+    }
+
+    public ArrayList<String> getUserJoined() {
+        return userJoined;
+    }
+
+    public void setUserJoined(ArrayList<String> userJoined) {
+        this.userJoined = userJoined;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    @Override
+    public String toString() {
+        return "EventModel{" +
+                "capacity=" + capacity +
+                ", description='" + description + '\'' +
+                ", eventCreated=" + eventCreated +
+                ", eventEnd=" + eventEnd +
+                ", eventStart=" + eventStart +
+                ", imagePath='" + imagePath + '\'' +
+                ", module='" + module + '\'' +
+                ", status='" + status + '\'' +
+                ", title='" + title + '\'' +
+                ", userCreated='" + userCreated + '\'' +
+                ", userJoined=" + userJoined +
+                ", venue='" + venue + '\'' +
+                '}';
+    }
+}
