f330713f6a1c6508954b1bbe83f33d9d384d4697
chuanshaof <chuanshaof@gmail.com>
2022-03-22 13:30:23 +0800

    add create event

---
+++ app/src/main/java/com/example/myapplication2/CreateEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
