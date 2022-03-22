5752d244108c1c7453b30aed487fac9a8773a79b
chuanshaof <chuanshaof@gmail.com>
2022-03-22 13:32:41 +0800

    add create event

--- app/src/main/java/com/example/myapplication2/CreateEventActivity.java
+++ app/src/main/java/com/example/myapplication2/CreateEventActivity.java
@@ -10,5 +10,6 @@ public class CreateEventActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
