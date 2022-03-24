71b022034c19623da55a659f33a510dd6c4b10b2
chuanshaof <chuanshaof@gmail.com>
2022-03-24 17:45:32 +0800

    add dependency
    still not sure how to use it

--- app/src/main/java/com/example/myapplication2/CreateEventActivity.java
+++ app/src/main/java/com/example/myapplication2/CreateEventActivity.java
@@ -6,14 +6,13 @@ import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -27,11 +26,14 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -47,20 +49,26 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
 
     Intent intent;
 
-    Uri selectedImage;
+    Uri selectedImageUri;
 
-    static final String TAG = "Create Events";
+    String currentPhotoPath;
+
+    static final String TAG = "CreateEvents";
 
     static final int REQUEST_ID_CAMERA = 1;
     static final int REQUEST_ID_STORAGE = 2;
     static final int REQUEST_CROP_PIC = 3;
+
+    // TODO: Check out glide and picasso for handling photos
+    // http://bumptech.github.io/glide/doc/download-setup.html
+    // https://square.github.io/picasso/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_events);
 
-
+        // Casting to ensure that the types are correct
         createButton = (Button) findViewById(R.id.createButton);
         locationImage = (ImageView) findViewById(R.id.locationImageCreate);
         eventTitleCreate = (EditText) findViewById(R.id.eventTitleCreate);
@@ -107,20 +115,44 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
     // Edited the part where user can still click and request individually
     // Function to let the user to choose image from camera or gallery
     private void chooseImage(Context context){
-        final CharSequence[] optionsMenu = {"Take Photo", "Choose from Gallery", "Exit" }; // create a menuOption Array
+        // create a menuOption Array
+        final CharSequence[] optionsMenu = {"Take Photo", "Choose from Gallery", "Exit" };
+
         // Create a dialog for showing the optionsMenu
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
         // Set the items in builder
         builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 if(optionsMenu[i].equals("Take Photo")){
                     if (checkAndRequestPermission(CreateEventActivity.this, Manifest.permission.CAMERA, REQUEST_ID_CAMERA)) {
+                        // Code adapted from:
+                        // https://developer.android.com/training/camera/photobasics#TaskGallery
                         // Implicit event access camera
-                        Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                        startActivityForResult(takePicture, REQUEST_ID_CAMERA);
+                        // Create file to save the photo to so that we are able to obtain the full-sized image in the end
+                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                        // Create the File where the photo should go
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                        } catch (IOException ex) {
+                            // Error occurred while creating the File
+                            Log.d(TAG, "File creation error");
+                        }
+
+                        // Continue only if the File was successfully created
+                        if (photoFile != null) {
+                            Uri photoURI = FileProvider.getUriForFile(CreateEventActivity.this,
+                                    "com.example.android.fileprovider",
+                                    photoFile);
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(takePictureIntent, REQUEST_ID_CAMERA);
+                        }
                     }
                 }
+
                 else if(optionsMenu[i].equals("Choose from Gallery")){
                     if (checkAndRequestPermission(CreateEventActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_ID_STORAGE)) {
                         // Implicit event to access storage
@@ -137,36 +169,51 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
     }
 
 
-    // Implicit event return and to obtain data
+    // Handle implicit intent calls
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode != RESULT_CANCELED) {
             switch (requestCode) {
                 case REQUEST_ID_CAMERA:
-                    if (resultCode == RESULT_OK && data != null) {
-                        Bitmap selectedImages = (Bitmap) data.getExtras().get("data");
+                    // Don't need to check for data != null as we never used it, but only to decode path
+                    if (resultCode == RESULT_OK) {
+                        // Decoding the bitmap into a URI so that we can use it in performCrop();
+                        Bitmap selectedImageBitmap = BitmapFactory.decodeFile(currentPhotoPath);
                         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                        selectedImages.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-                        String path = MediaStore.Images.Media.insertImage(getContentResolver(), selectedImages, "Title", null);
-                        selectedImage = Uri.parse(path);
-                        performCrop();
+                        selectedImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                        String path = MediaStore.Images.Media.insertImage(getContentResolver(), selectedImageBitmap, "Title", null);
+                        selectedImageUri = Uri.parse(path);
 
+                        performCrop();
                     }
                     break;
                 case REQUEST_ID_STORAGE:
                     if (resultCode == RESULT_OK && data != null) {
-                        selectedImage = data.getData();
+                        // Getting URI from the intent
+                        // TODO: Currently having bugs when running on emulator, possibly due to getting it from file explorer rather than gallery?
+                        //       possible fix might include saving the photo, but not ideal because it will keep creating in memory
+                        selectedImageUri = data.getData();
                         performCrop();
+
                     }
                     break;
                 case REQUEST_CROP_PIC:
                     if (resultCode == RESULT_OK && data != null) {
                         // get the returned data
                         Bundle extras = data.getExtras();
+                        Uri uri = data.getData();
+                        Log.d(TAG, "onActivityResult: "+ uri.toString());
+
+
                         // get the cropped bitmap
-                        Bitmap thePic = extras.getParcelable("data");
-                        locationImage.setImageBitmap(thePic);
+                        // TODO: for emulator, find out why extras is always null
+                        if (extras != null) {
+                            Bitmap photo = extras.getParcelable("data");
+                            locationImage.setImageBitmap(photo);
+                        } else {
+                            Log.d(TAG, "onCropActivityResult: failed");
+                        }
                     }
             }
         }
@@ -174,12 +221,14 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
 
     // Crop images
     private void performCrop() {
-        // take care of exceptions
+        // take care of exceptions when the device does not have a crop action
         try {
-            // call the standard crop action intent (the user device may not support it)
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            // Call the standard crop action intent (the user device may not support it)
             Intent cropIntent = new Intent("com.android.camera.action.CROP");
             // indicate image type and Uri
-            cropIntent.setDataAndType(selectedImage, "image/*");
+            cropIntent.setDataAndType(selectedImageUri, "image/*");
             // set crop properties
             cropIntent.putExtra("crop", "true");
             // indicate aspect of desired crop
@@ -189,14 +238,14 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
             cropIntent.putExtra("outputX", 256);
             cropIntent.putExtra("outputY", 256);
             // retrieve data on return
+            // TODO: might have bugs here for emulator
             cropIntent.putExtra("return-data", true);
             // start the activity - we handle returning in onActivityResult
             startActivityForResult(cropIntent, REQUEST_CROP_PIC);
         }
         // respond to users whose devices do not support the crop action
         catch (ActivityNotFoundException anfe) {
-            Toast toast = Toast
-                    .makeText(this, "This device doesn't support the crop action!", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(this, "This device doesn't support the crop action!", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
@@ -224,30 +273,72 @@ public class CreateEventActivity extends AppCompatActivity implements View.OnCli
         return true;
     }
 
-    // Handled permission results
-    // Adapted to go right into the implicit intent if it is allowed
+    // Handle permission results after ActivityCompat.requestPermissions()
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // TODO: try and modularize this code as it is repeated from the top & possibly in other activities
+        //       also to fix the possibility of changing a code and being able to change it everywhere also
+        // NOTE: possible issue with trying to modularize it with other activities as this is an Overriden function
+        //       possible fix might be to create another class to do this??
         switch (requestCode) {
             case REQUEST_ID_CAMERA:
-                // if permission is not granted
+                // If camera access is still denied after requesting
                 if (ContextCompat.checkSelfPermission(CreateEventActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(getApplicationContext(), R.string.camera_access, Toast.LENGTH_SHORT).show();
                 } else {
-                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(takePicture, requestCode);
+                    // Implicit event access camera
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        Log.d(TAG, "File creation error");
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(CreateEventActivity.this,
+                                "com.example.android.fileprovider",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, REQUEST_ID_CAMERA);
+                    }
                 }
                 break;
             case REQUEST_ID_STORAGE:
                 if (ContextCompat.checkSelfPermission(CreateEventActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(getApplicationContext(), R.string.storage_access, Toast.LENGTH_SHORT).show();
                 } else {
-                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(pickPhoto , requestCode);
+                    // Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, requestCode);
                 }
                 break;
         }
+    }
+
+    // Function to create an empty image file so that we are able to save the full photo to later
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",    /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void cropPicture() {
     }
 
 }
