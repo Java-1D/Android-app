b117ff99bd862be708c678bbf3747b3da6187f07
Yongkang <chiayongkang@hotmail.com>
2022-03-27 22:16:56 +0800

    changed EventModel

--- app/src/main/java/com/example/myapplication2/objectmodel/EventModel.java
+++ app/src/main/java/com/example/myapplication2/objectmodel/EventModel.java
@@ -7,22 +7,22 @@ import java.util.Arrays;
 import java.util.Date;
 
 /*
- * Firebase Firestore Document Object Model for the Events Collection
- * @field capacity: number
- * @field description: string
- * @field eventCreated: timestamp
- * @field eventEnd: timestamp
- * @field eventStart: timestamp
- * @field imagePath: DocumentReference from Images Collection
- * @field lastUpdated: timestamp
- * @field module: DocumentReference from Modules Collection
- * @field status: string
- * @field title: string
- * @field userCreated: DocumentReference from Users Collection
- * @field userJoined: ArrayList of DocumentReference from Users Collection
- *        @index: DocumentReference from Users Collection
- * @field venue: DocumentReference from Venues Collection
- */
+* Firebase Firestore Document Object Model for the Events Collection
+* @field capacity: number
+* @field description: string
+* @field eventCreated: timestamp
+* @field eventEnd: timestamp
+* @field eventStart: timestamp
+* @field imagePath: DocumentReference from Images Collection
+* @field lastUpdated: timestamp
+* @field module: DocumentReference from Modules Collection
+* @field status: string
+* @field title: string
+* @field userCreated: DocumentReference from Users Collection
+* @field userJoined: ArrayList of DocumentReference from Users Collection
+*        @index: DocumentReference from Users Collection
+* @field venue: DocumentReference from Venues Collection
+*/
 public class EventModel {
 
     private final ArrayList<String> statuses = new ArrayList<>(Arrays.asList("upcoming", "ongoing", "completed"));
@@ -45,18 +45,11 @@ public class EventModel {
 
     public EventModel() {} //no arg constructor for firebase
 
-<<<<<<< HEAD
     public EventModel(int capacity, String description, Date eventCreated,
                       Date eventEnd, Date eventStart, DocumentReference imagePath, Date lastUpdated,
                       DocumentReference module, String status, String title,
                       DocumentReference userCreated, ArrayList<DocumentReference> userJoined,
                       DocumentReference venue) {
-=======
-    public EventModel() {
-    } //no arg constructor for firebase
-
-    public EventModel(int capacity, String description, Date event_created, Date event_end, Date event_start, DocumentReference image_path, Date last_updated, DocumentReference module, String status, String title, DocumentReference user_created, ArrayList<DocumentReference> user_joined, DocumentReference venue) {
->>>>>>> working with images
         this.capacity = capacity;
         this.description = description;
         this.eventCreated = eventCreated;
@@ -71,7 +64,6 @@ public class EventModel {
         this.userJoined = userJoined;
         this.venue = venue;
     }
-
 
     public int getCapacity() {
         return capacity;
@@ -89,21 +81,12 @@ public class EventModel {
         this.description = description;
     }
 
-<<<<<<< HEAD
     public Date getEventCreated() {
         return eventCreated;
     }
 
     public void setEventCreated(Date eventCreated) {
         this.eventCreated = eventCreated;
-=======
-    public Date getEvent_created() {
-        return event_created;
-    }
-
-    public void setEvent_created(Date event_created) {
-        this.event_created = event_created;
->>>>>>> working with images
     }
 
     public Date getEventEnd() {
@@ -122,34 +105,20 @@ public class EventModel {
         this.eventStart = eventStart;
     }
 
-<<<<<<< HEAD
     public DocumentReference getImagePath() {
         return imagePath;
-=======
-    public DocumentReference getImage_path() {
-        return image_path;
->>>>>>> working with images
     }
 
     public void setImagePath(DocumentReference imagePath) {
         this.imagePath = imagePath;
     }
 
-<<<<<<< HEAD
     public Date getLastUpdated() {
         return lastUpdated;
     }
 
     public void setLastUpdated(Date lastUpdated) {
         this.lastUpdated = lastUpdated;
-=======
-    public Date getLast_updated() {
-        return last_updated;
->>>>>>> working with images
-    }
-
-    public DocumentReference getModule() {
-        return module;
     }
 
     public DocumentReference getModule() {
@@ -189,21 +158,12 @@ public class EventModel {
         this.title = title;
     }
 
-<<<<<<< HEAD
     public DocumentReference getUserCreated() {
         return userCreated;
     }
 
     public void setUserCreated(DocumentReference userCreated) {
         this.userCreated = userCreated;
-=======
-    public DocumentReference getUser_created() {
-        return user_created;
-    }
-
-    public void setUser_created(DocumentReference user_created) {
-        this.user_created = user_created;
->>>>>>> working with images
     }
 
     public ArrayList<DocumentReference> getUserJoined() {
@@ -227,7 +187,6 @@ public class EventModel {
         return "EventModel{" +
                 "capacity=" + capacity +
                 ", description='" + description + '\'' +
-<<<<<<< HEAD
                 ", eventCreated=" + eventCreated +
                 ", eventEnd=" + eventEnd +
                 ", eventStart=" + eventStart +
@@ -238,18 +197,6 @@ public class EventModel {
                 ", title='" + title + '\'' +
                 ", userCreated=" + userCreated +
                 ", userJoined=" + userJoined +
-=======
-                ", event_created=" + event_created +
-                ", event_end=" + event_end +
-                ", event_start=" + event_start +
-                ", image_path=" + image_path +
-                ", last_updated=" + last_updated +
-                ", module=" + module +
-                ", status='" + status + '\'' +
-                ", title='" + title + '\'' +
-                ", user_created=" + user_created +
-                ", user_joined=" + user_joined +
->>>>>>> working with images
                 ", venue=" + venue +
                 '}';
     }
