37b7b8984fbce7abee7b155cc35ac410f2664146
issacj-17 <issac_jose@mymail.sutd.edu.sg>
2022-03-31 10:11:41 +0800

    Update Utils.java
    Added circle crop to work with Picasso API to crop circle image for ProfilePicture

--- app/src/main/java/com/example/myapplication2/utils/Utils.java
+++ app/src/main/java/com/example/myapplication2/utils/Utils.java
@@ -2,11 +2,16 @@ package com.example.myapplication2.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.media.Image;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -37,5 +42,40 @@ public class Utils {
 
     public static void loadImage(String url, ImageView imageView) {
         Picasso.get().load(url).into(imageView);
+    }
+
+    public static class CircleTransform implements Transformation {
+        @Override
+        public Bitmap transform(Bitmap source) {
+            int size = Math.min(source.getWidth(), source.getHeight());
+
+            int x = (source.getWidth() - size) / 2;
+            int y = (source.getHeight() - size) / 2;
+
+            Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+            if (squaredBitmap != source) {
+                source.recycle();
+            }
+
+            Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            BitmapShader shader = new BitmapShader(squaredBitmap,
+                    Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+            paint.setShader(shader);
+            paint.setAntiAlias(true);
+
+            float r = size / 2f;
+            canvas.drawCircle(r, r, r, paint);
+
+            squaredBitmap.recycle();
+            return bitmap;
+        }
+
+        @Override
+        public String key() {
+            return "circle";
+        }
     }
 }
