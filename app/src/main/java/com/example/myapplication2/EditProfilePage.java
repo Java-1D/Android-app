b4c0b4eb7c68350d94882d16fc05eb7ac9d2f0bd
issacj-17 <issac_jose@mymail.sutd.edu.sg>
2022-04-01 23:34:31 +0800

    Update EditProfilePage.java
    Refactored onClickListener as an inner class to keep code clean on EditProfilePage

--- app/src/main/java/com/example/myapplication2/EditProfilePage.java
+++ app/src/main/java/com/example/myapplication2/EditProfilePage.java
@@ -23,6 +23,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.myapplication2.objectmodel.ProfileModel;
+import com.example.myapplication2.objectmodel.UserModel;
+import com.example.myapplication2.utils.Container;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -30,13 +33,11 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.Date;
 
 //TODO Data Persistence for EditProfilePage
-public class EditProfilePage extends AppCompatActivity implements View.OnClickListener {
+public class EditProfilePage extends AppCompatActivity {
     private static final String TAG = "EditProfilePage";
-    FirebaseFirestore db;
 
     enum Data {
         NAME,
-        EMAIL,
         PILLAR,
         TERM,
         MODULE,
@@ -46,37 +47,83 @@ public class EditProfilePage extends AppCompatActivity implements View.OnClickLi
     ImageView profilePicture;
     ImageView backButton;
     EditText editName;
-    EditText editEmail;
     EditText editPillar;
     EditText editTerm;
     EditText editModules;
     EditText editBio;
     Button confirmEdit;
 
+    //Objects to handle data from Firebase
+    FirebaseFirestore db;
+    String profileDocumentId;
+    final Container<ProfileModel> profile = new Container<>(new ProfileModel());
+
+    class ClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.editProfilePicture:
+                    chooseProfilePic();
+                    break;
+                case R.id.confirmButton:
+                    if (!editName.getText().toString().matches("")) {
+                        updateProfileDataString(profileDocumentId, Data.NAME, editName.getText().toString());
+                    }
+                    if (!editPillar.getText().toString().matches("")) {
+                        updateProfileDataString(profileDocumentId, Data.PILLAR, editPillar.getText().toString());
+                    }
+                    if (!editTerm.getText().toString().matches("")) {
+                        updateProfileDataNumber(profileDocumentId, Data.TERM, Long.parseLong(editTerm.getText().toString()));
+                    }
+                    if (!editBio.getText().toString().matches("")) {
+                        updateProfileDataString(profileDocumentId, Data.BIO, editBio.getText().toString());
+                    }
+                    startActivity(new Intent(EditProfilePage.this, ProfilePage.class));
+                    break;
+                case R.id.backButton:
+                    startActivity(new Intent(EditProfilePage.this, MainPageActivity.class));
+                    break;
+
+            }
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                profileDocumentId = extras.getString("PROFILE_ID");
+            }
+            else {
+                profileDocumentId = null;
+                Log.w(TAG, "Intent is empty.");
+            }
+        }
+        else {
+            profileDocumentId = null;
+            Log.w(TAG, "SavedInstanceState is empty.");
+        }
+
+        profileDocumentId = "Test";
+
         setContentView(R.layout.activity_edit_profile_page);
 
         profilePicture = findViewById(R.id.editProfilePicture);
         backButton = findViewById(R.id.backButton);
         editName = findViewById(R.id.editName);
-        editEmail = findViewById(R.id.editEmail);
         editPillar = findViewById(R.id.editPillar);
         editTerm = findViewById(R.id.editTerm);
         editModules = findViewById(R.id.editModules);
         editBio = findViewById(R.id.editBio);
         confirmEdit = findViewById(R.id.confirmButton);
 
-        profilePicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chooseProfilePic();
-            }
-        });
-
-        confirmEdit.setOnClickListener(this);
-        backButton.setOnClickListener(this);
+        profilePicture.setOnClickListener(new ClickListener());
+        confirmEdit.setOnClickListener(new ClickListener());
+        backButton.setOnClickListener(new ClickListener());
     }
 
     @Override
@@ -110,16 +157,23 @@ public class EditProfilePage extends AppCompatActivity implements View.OnClickLi
     }
 
     private void chooseProfilePic() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        View dialogview = inflater.inflate(R.layout.add_picture_alert, null);
+        View dialogview;
+        AlertDialog.Builder builder;
+        AlertDialog alertDialogProfilePicture;
+        LayoutInflater inflater;
+        ImageView takePic;
+        ImageView chooseGallery;
+
+        builder = new AlertDialog.Builder(this);
+        inflater = getLayoutInflater();
+        dialogview = inflater.inflate(R.layout.add_picture_alert, null);
         builder.setCancelable(false);
         builder.setView(dialogview);
 
-        ImageView takePic = dialogview.findViewById(R.id.takePic);
-        ImageView chooseGallery = dialogview.findViewById(R.id.chooseGallery);
+        takePic = dialogview.findViewById(R.id.takePic);
+        chooseGallery = dialogview.findViewById(R.id.chooseGallery);
 
-        AlertDialog alertDialogProfilePicture = builder.create();
+        alertDialogProfilePicture = builder.create();
         alertDialogProfilePicture.show();
 
         takePic.setOnClickListener(new View.OnClickListener() {
@@ -197,34 +251,6 @@ public class EditProfilePage extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.confirmButton:
-                if (!editName.getText().toString().matches("")) {
-                    updateProfileDataString("Test", Data.NAME, editName.getText().toString());
-                }
-                if (!editEmail.getText().toString().matches("")) {
-                    updateUserDataString("Test", Data.EMAIL, editEmail.getText().toString());
-                }
-                if (!editPillar.getText().toString().matches("")) {
-                    updateProfileDataString("Test", Data.PILLAR, editPillar.getText().toString());
-                }
-                if (!editTerm.getText().toString().matches("")) {
-                    updateProfileDataNumber("Test", Data.TERM, Long.parseLong(editTerm.getText().toString()));
-                }
-                if (!editBio.getText().toString().matches("")) {
-                    updateProfileDataString("Test", Data.BIO, editBio.getText().toString());
-                }
-                startActivity(new Intent(EditProfilePage.this, ProfilePage.class));
-                break;
-            case R.id.backButton:
-                startActivity(new Intent(EditProfilePage.this, MainPageActivity.class));
-                break;
-
-        }
-    }
-
     public void updateProfileDataString(String Id, Data data, String value) {
         Date date = new Date();
         Timestamp current = new Timestamp(date);
@@ -258,22 +284,6 @@ public class EditProfilePage extends AppCompatActivity implements View.OnClickLi
                 Log.i(TAG, "Update Term");
                 break;
             default:
-
-        }
-        docRef.update("profileUpdated", current);
-    }
-
-    public void updateUserDataString(String Id, Data data, String value) {
-        Date date = new Date();
-        Timestamp current = new Timestamp(date);
-        DocumentReference docRef = db.collection("Users").document(Id);
-        switch (data) {
-            case EMAIL:
-                docRef.update("email", value);
-                Log.i(TAG, "Update Email");
-                break;
-            default:
-
 
         }
         docRef.update("profileUpdated", current);
