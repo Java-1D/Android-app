a73c49ea5428b618791cc6017f25b2804873a734
issacj-17 <issac_jose@mymail.sutd.edu.sg>
2022-04-12 16:55:43 +0800

    Update ExploreFragment.java

--- app/src/main/java/com/example/myapplication2/fragments/ExploreFragment.java
+++ app/src/main/java/com/example/myapplication2/fragments/ExploreFragment.java
@@ -25,14 +25,32 @@ import com.example.myapplication2.utils.Utils;
 import com.example.myapplication2.viewholder.EventViewHolder;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class ExploreFragment extends Fragment {
-    private static final String TAG = "HOMEFRAGMENT";
-    private FirebaseFirestore firebaseFirestore;
+    private static final String TAG = "ExploreFragment";
+
+    //Objects to handle data from Firebase
+    FirebaseFirestore db;
+    Query query;
+
+    //RecyclerView
     private RecyclerView eventsList; // providing views that represent items in a data set.
     private FirestoreRecyclerAdapter adapter;
+
+    //Store Hashmap of name, DocumentReference to update ArrayList of Modules
+    Map<String, DocumentReference> modulesMap = new HashMap<>();
 
     public ExploreFragment() {
         // Required empty public constructor
@@ -73,13 +91,78 @@ public class ExploreFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        firebaseFirestore = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        //TODO: Replace Query object from Filter Activity if there is no null values passed via intent
         // Query
-        Query query = firebaseFirestore.collection("Events");
+        //TODO: Replace Query object from Filter Activity if there is no null values passed via intent
+        query = db.collection("Events");
         Log.d(TAG, "Query" + query.toString());
+        buildFirestoreRecyclerView(query);
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "OnStart is called");
+        adapter.startListening();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume is called");
+
+        Intent intent = getActivity().getIntent();
+        String modulePath = intent.getStringExtra("MODULE_SELECTION");
+        int capacity = intent.getIntExtra("CAPACITY_SELECTION", 0);
+
+        if (modulePath != null && capacity != 0) {
+             query = filterEvents(modulePath, capacity);
+        } else if (modulePath != null) {
+            query = filterEvents(modulePath);
+        } else if (capacity != 0) {
+            query = filterEvents(capacity);
+        } else {
+            query = db.collection("Events");
+            Log.d(TAG, "Query" + query.toString());
+        }
+        buildFirestoreRecyclerView(query);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop is called");
+        adapter.stopListening();
+    }
+
+    //FIXME: Move Methods to ExploreFragment to perform query based on values passed in Filter Button via intent
+    protected Query filterEvents(String moduleSelection) {
+        DocumentReference moduleDocRef = db.document(moduleSelection);
+
+        Query query = db.collection("Events").whereEqualTo("modules", moduleDocRef);
+        Log.d(TAG, "Query" + query.toString());
+        return query;
+    }
+
+    protected Query filterEvents(int capacitySelection) {
+        //Filter based on Capacity Chosen -> Checks whether an events have at least x slots available for user and his friends to join
+        Query query = db.collection("Events").whereGreaterThanOrEqualTo("capacity", capacitySelection);
+        Log.d(TAG, "Query" + query.toString());
+        return query;
+    }
+
+    protected Query filterEvents(String moduleSelection, int capacitySelection) {
+        DocumentReference moduleDocRef = db.document(moduleSelection);
+
+        //TODO: Need to check whether compound queries such as the one below works
+        //Filter based on Capacity Chosen -> Checks whether an events have at least x slots available for user and his friends to join
+        Query query = db.collection("Events").whereEqualTo("modules", moduleDocRef).whereGreaterThanOrEqualTo("capacity", capacitySelection);
+        Log.d(TAG, "Query" + query.toString());
+        return query;
+    }
+
+    void buildFirestoreRecyclerView(Query query) {
         FirestoreRecyclerOptions<EventModel> options = new FirestoreRecyclerOptions.Builder<EventModel>()
                 .setQuery(query, EventModel.class)
                 .build();
@@ -123,17 +206,4 @@ public class ExploreFragment extends Fragment {
 
         };
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        adapter.stopListening();
-    }
-
 }
