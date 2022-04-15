1c3f47cfa4c008c3763716354b2f7a69e540a11d
issacj-17 <issac_jose@mymail.sutd.edu.sg>
2022-04-15 21:11:18 +0800

    Cleaned up and Refactored Code for calls to Firebase
    Removed RecyclerViewModel and RecyclerContactAdaptor as it was not used by any activity
    Removed BackgroundTask and Container
    Created FirebaseDocument java class file

---
+++ app/src/main/java/com/example/myapplication2/utils/FirebaseDocument.java
@@ -0,0 +1,50 @@
+package com.example.myapplication2.utils;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public abstract class FirebaseDocument {
+    private static final String TAG = "FirebaseDocument";
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private DocumentReference getDocumentReference(String collectionId, String documentId) {
+        return db.collection(collectionId).document(documentId);
+    }
+
+    private void getData(DocumentReference docRef) {
+        Log.i(TAG, "File Path in Firebase: " + docRef.getPath());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot document) {
+                if (document.exists()) {
+                    callbackOnSuccess(document);
+                } else {
+                    Log.w(TAG, "Document does not exist");
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error retrieving document from Firestore", e);
+            }
+        });
+    }
+
+    public abstract void callbackOnSuccess(DocumentSnapshot document);
+
+    public void run(String collectionId, String documentId) {
+        DocumentReference docRef = getDocumentReference(collectionId, documentId);
+        getData(docRef);
+    }
+
+    public void run(DocumentReference docRef) {
+        getData(docRef);
+    }
+}
